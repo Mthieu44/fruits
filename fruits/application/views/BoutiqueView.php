38806e5255a9c3d765205e70e6afb5cde36ecd56
Sylvain BAUDOUIN <E216351P@univ-nantes.fr>
2022-11-10 09:30:07 +0100

    All View in php

---
+++ fruits/application/views/BoutiqueView.php
@@ -0,0 +1,42 @@
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Fruits • Boutique</title>
+
+    
+  <style>
+  <?php include 'css/style.css'; ?>
+  <?php include 'css/home.css'; ?>
+  </style>
+  
+  </head>
+
+    <body>
+        <header>
+            <nav>
+                <ul>
+                <li class="logo">
+                    <img src="<?= base_url('img/logo.png') ?>" alt="fruit" class="logo2" />
+                    <h1><b class="yellow">F</b>RUITS</h1>
+                </li>
+                <li><a href="<?=site_url('Home')?>" >Accueil</a></li>
+                <li><a href=" <?=site_url('Boutique')?>" class="yellow" >Boutique</a></li>
+                <li><a href="<?=site_url('APropos')?>"  >A propos</a></li>
+                <li><a href="<?=site_url('Contact')?>">Contact</a></li>
+                <li class="connexion">
+                    <img src="<?= base_url('img/bonhomme.png')?>" alt="connexion" />
+                    <a href="<?=site_url('Connexion')?>">Connexion</a> 
+                </li>
+                <li class="panier">
+                    <a href="<?=site_url('Panier')?>" >
+                        <img src="<?= base_url('img/panier.png')?>" alt="panier"  />
+                        <div><p>0</p></div>
+                    </a>
+                </li>
+            </ul>
+            </nav>
+        </header>
+    </body>
+</html>
