0793c0f2662fa0563e3aaf1d356e05b1fe5cfdf7
= <Ethan27349570!>
2022-12-01 22:23:15 +0100

    vue js

---
+++ fruits/js/panierVues.js
@@ -0,0 +1,57 @@
+const vue = new Vue({
+    data : () => {
+        return {
+            fruits : [],
+            panier : [],
+            url : "http://localhost/public_html/equipe2-1/fruits/",
+        }
+    },
+    methods : {
+        ajouterAuPanier(id) {
+            for (let i = 0; i < this.fruits.length; i++) {
+                if (this.fruits[i].id == id) {
+                    this.panier.push(this.fruits[i]);
+                    break;
+                }
+            }
+        },
+        retirerDuPanier(id) {
+            for (let i = 0; i < this.panier.length; i++) {
+                if (this.panier[i].id == id) {
+                    this.panier.splice(i, 1);
+                    break;
+                }
+            }
+        },
+        totalQuantity(n,id){
+            for (let i = 0; i < this.fruits.length; i++){
+                if (this.fruits[i].id_fruit == id){
+                    this.fruits[i].quantity += n;
+                    
+                    if (this.fruits[i].quantity < 0){
+                        this.fruits[i].quantity = 0;
+                    }
+                }
+            }
+        },
+        getImg(id){
+            for (let i = 0; i < this.fruits.length; i++){
+                if (this.fruits[i].id_fruit == id){
+                    return this.url.concat("/img/fruit/").concat(this.fruits[i].image);
+                }
+            }
+        },
+    }, 
+    mounted() {
+        axios.get(url.concat('index.php/panier/getPanier'))
+        .then((res) => res.data)    
+        .then((res) => {
+            this.panier = res;
+        })
+        axios.get(url.concat('index.php/panier/getAllFruits'))
+        .then((res) => res.data)
+        .then((res) => {
+            this.fruits = res;
+        })
+    },  
+}).$mount('#app-vue');
