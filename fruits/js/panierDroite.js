9dce3df78630225c96e6e0f31419fe91972638ec
E216439K <tom.freret@etu.univ-nantes.com>
2023-01-03 14:25:27 +0100

    yes

--- fruits/js/panierDroite.js
+++ fruits/js/panierDroite.js
@@ -1,6 +1,6 @@
 let angle = 0
 
-function showPanier(){
+function showPanier() {
     let volet = document.getElementById("panierVolet");
     volet.style.right = 0;
     let fleche = document.getElementById("voletFlecheImage");
@@ -10,32 +10,32 @@ function showPanier(){
     let hider = document.getElementById("hider");
     hider.style.display = "block";
     let blurring = document.getElementsByClassName("blur")
-    for (elem in blurring){
+    for (elem in blurring) {
         blurring[elem].style.filter = "blur(2px)"
     }
 
 }
 
-function closePanier(){
+function closePanier() {
     let volet = document.getElementById("panierVolet");
-    volet.style.right = "-20%"; 
+    volet.style.right = "-400px";
     let fleche = document.getElementById("voletFlecheImage");
     angle += 180
     fleche.style.transform = `rotate(${angle}deg)`;
     let hider = document.getElementById("hider");
     hider.style.display = "none";
     let blurring = document.getElementsByClassName("blur")
-    for (elem in blurring){
+    for (elem in blurring) {
         blurring[elem].style.filter = "none"
     }
 }
 
-function flechePanier(){
+function flechePanier() {
     let volet = document.getElementById("panierVolet");
-    if (volet.style.right == '0px'){
+    if (volet.style.right == '0px') {
         closePanier()
     }
-    else{
+    else {
         showPanier()
     }
 }
